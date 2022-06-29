4de1b65430ffe99916816c132ceedef5efbd3af8
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-06-29 10:38:03 +0100

    if else loop

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 for x in range(97, 123):
     don = chr(x)
     print("{}".format(don), end='')
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 for x in range(0, 100):
     if x <= 9:
         print("{}".format(x), end=', ')
