06b2e7fa51dc813e32cd408f593da777edbec281
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-06-30 18:54:30 +0100

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,47 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
-    from calculator_1 import add as sum, sub as diff, mul as dup, div as res
-    from sys import argv, exit
-    ops = ['+', '-', '*', '/']
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>", end='\n')
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    count = len(argv)
+
+    if count != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
-        
-    a = int(argv[1])
-    b = int(argv[3])
-    operator = argv[2]
-    if operator == ops[0]:
-        result = sum(a, b)
-    elif operator == ops[1]:
-        result = diff(a, b)
-    elif operator == ops[2]:
-        result = dup(a, b)
-    elif operator == ops[3]:
-        result = res(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /", end='\n')
+
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+    op = argv[2]
+
+    def not_found():
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    print("{} {} {} = {}".format(a, operator, b, result))
-    exit(0)
-    
-        
+
+    def my_add():
+        total = add(num1, num2)
+        print("{:d} + {:d} = {:d}".format(num1, num2, total))
+        return total
+
+    def my_sub():
+        total = sub(num1, num2)
+        print("{:d} - {:d} = {:d}".format(num1, num2, total))
+        return total
+
+    def my_mul():
+        total = mul(num1, num2)
+        print("{:d} * {:d} = {:d}".format(num1, num2, total))
+        return total
+
+    def my_div():
+        total = div(num1, num2)
+        print("{:d} / {:d} = {:d}".format(num1, num2, total))
+        return total
+
+    options = {
+        "+": my_add,
+        "-": my_sub,
+        "*": my_mul,
+        "/": my_div
+    }
+    options.get(op, not_found)()
     
     
