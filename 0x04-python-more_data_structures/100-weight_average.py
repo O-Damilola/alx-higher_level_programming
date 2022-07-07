684eeed58163d4f360ad259c9334f3ca337c116d
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-07-07 08:33:29 -0700

    weight average roman to integer

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,10 +2,10 @@
 def weight_average(my_list=[]):
     sum = 0
     product = 0
+    if len(my_list) == 0:
+        return 0
     for k in my_list:
         product += k[0] * k[1]
         sum += k[1]
-    print(sum)
-    print(product)
-    average = product / sum
+    average = product/sum
     return average
