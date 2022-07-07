ef788e203326c3908acc52e103b3eb2a23a37c67
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-07-07 15:55:11 +0100

    weight average and roman to integer revised

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,15 +3,33 @@ def roman_to_int(roman_string):
     int_list = []
     int_sum = 0
     roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-    str_list = list(roman_string)
-    for i in str_list:
-        int_list.append(roman[i])
-    j = 0
-    while j < len(int_list)-1:
-        if int_list[j] < int_list[j+1]:
-            int_sum += -int_list[j]
-        else:
-            int_sum += int_list[j]
-        j += 1
-    int_sum += int_list[-1]       
-    return(int_sum)
+    test = type(roman_string) != str
+    if type(roman_string) is str and roman_string:
+        str_list = list(roman_string)
+        for i in str_list:
+            int_list.append(roman[i])
+        j = 0
+        while j < len(int_list)-1:
+            if int_list[j] < int_list[j+1]:
+                int_sum += -int_list[j]
+            else:
+                int_sum += int_list[j]
+            j += 1
+        int_sum += int_list[-1]       
+        return (int(int_sum))
+    return 0
+
+roman_number = None
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = 'VII'
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
