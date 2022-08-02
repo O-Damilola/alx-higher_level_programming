45c68e5584e72c87db1869c85099daa4eb975e86
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-08-02 09:04:51 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as don:
-        for line in don:
+    """ function that reads a file """
+    with open(filename, encoding="utf-8") as jon:
+        """ Opens the file to be read """
+        for line in jon:
             print(line, end="")
